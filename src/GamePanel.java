0c1fd18527ed4d859cf22c634ad2ed0deaf119d1
hbreck <hbreckenridge@yahoo.com>
2023-07-24 10:48:08 -0400

    Setting Up Game environment for Snake game in GamePanel class

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,10 +5,26 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.*;
+import java.util.Timer;
 
 public class GamePanel extends JPanel implements ActionListener {
-    public static int SCREEN_WIDTH = 600;
-    public static int SCREEN_HEIGHT = 600;
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_WIDTH)/UNIT_SIZE;
+    static final int DELAY = 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+
+
 
     GamePanel() {
 
