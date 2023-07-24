1d9d85814c51e75e7c64589b771889cb9f7477f8
hbreck <hbreckenridge@yahoo.com>
2023-07-24 09:21:35 -0400

    Setting Up GameFrame class

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,6 +3,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel() {
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
