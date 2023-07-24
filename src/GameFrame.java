3d4f7bdf513a021704c74e9915c26a4d9b45f580
hbreck <hbreckenridge@yahoo.com>
2023-07-24 14:16:29 -0400

    Completed snake game pushing to Master

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,15 +1,21 @@
-import javax.swing.*;
 
-public class GameFrame extends JFrame {
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+    /**
+     *
+     */
+    // private static final long serialVersionUID = 1L;
+
     GameFrame() {
-
         this.add(new GamePanel());
-        this.setTitle("Snake");
+        this.setTitle("SNAKE");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
-
     }
+
 }
